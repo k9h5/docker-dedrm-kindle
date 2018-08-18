c303b7467fa07c751ab0fc0c586d5ed678e7a09c
k9h <k9h@localhost>
2018-08-18 16:55:24 +0900

    Add convert.sh and Modify README

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # docker-dedrm-kindle
 
-## How to use
+## How to use (Recommended)
+1. [Extract your kindle key](https://github.com/apprenticeharper/DeDRM_tools/blob/b1d13f2b/src/DeDRM_Kindle%20for%20Mac%20and%20PC%20Key_Help.htm)
+2. Get your kindle content file (*.azw) bounds with your key
+3. `./convert.sh keyfilepath srcfilepath dstfilepath`
+4. Read dstfilepath or convert other formats
+
+
+## Use docker image directly
 
 1. [Extract your kindle key](https://github.com/apprenticeharper/DeDRM_tools/blob/b1d13f2b/src/DeDRM_Kindle%20for%20Mac%20and%20PC%20Key_Help.htm) and name it as 'default_key.k4i'
 2. Get your kindle content file (*.azw) bounds with your key
 3. `docker container run -v <<DIRECTORY_ABSOLUTE_PATH_OF_content_file>>:/root/out -v <<DIRECTORY_ABSOLUTE_PATH_OF_default_key.k4i>>:/root/.DeDRMPrefs -it k9h5/dedrm-kindle '/root/out/<<content_file>>.azw'`
 4. Read <<content_file>>_nodrm.azw3 or convert other formats
+
+## Security
+
+Contents inside Docker image is proved by [https://hub\.docker\.com/r/k9h5/dedrm\-kindle/builds/](https://hub.docker.com/r/k9h5/dedrm-kindle/builds/), so you don't have to be afraid of key sniffing or other security risks.
---
+++ convert.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -eu
+
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 keyfilepath srcfilepath dstfilepath" 1>&2
+  exit 1
+fi
+
+# need absolute path because of docker mount point
+keyfilepath=$(readlink -f $1)
+srcfilepath=$(readlink -f $2)
+dstfilepath=$(readlink -f $3)
+
+if [[ $keyfilepath != *.k4i ]]; then
+  echo "keyfile: $keyfilepath should be end with .k4i" 1>&2
+fi
+if [[ $srcfilepath != *.azw ]]; then
+  echo "srcfile: $srcfilepath should be end with .azw" 1>&2
+fi
+if [[ $dstfilepath != *.azw3 ]]; then
+  echo "dstfile: $dstfilepath should be end with .azw3" 1>&2
+fi
+
+srcdirpath=$(dirname $srcfilepath)
+srcfilename=$(basename $srcfilepath)
+dstdirpath=$(dirname $dstfilepath)
+dstfilename=$(basename $dstfilepath)
+tmpkeydirpath=$(mktemp -d)
+
+# work around for https://github.com/apprenticeharper/DeDRM_tools/blob/b1d13f2b/src/k4mobidedrm.py#L265-L274
+orig_srcfilename=$srcfilename
+srcfilename=tmpname.azw
+ln -s $orig_srcfilename $srcdirpath/$srcfilename
+
+cp $keyfilepath $tmpkeydirpath
+
+image=k9h5/dedrm-kindle
+docker container run -v $dstdirpath:/root/out -v $srcdirpath:/root/in -v $tmpkeydirpath:/root/.DeDRMPrefs -it $image /root/in/$srcfilename
+
+mv $dstdirpath/tmpname_nodrm.azw3 $dstdirpath/$dstfilename
+
+# work around for https://github.com/apprenticeharper/DeDRM_tools/blob/b1d13f2b/src/k4mobidedrm.py#L265-L274
+if [[ -L $srcdirpath/$srcfilename ]]; then
+  rm $srcdirpath/$srcfilename
+fi
