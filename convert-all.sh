c69d3a2a486c1637f810b3ac0e22a20bd20f3dc3
k9h5 <k9h5@localhost>
2018-08-19 12:44:31 +0900

    Fix for paths with whitespace

--- convert-all.sh
+++ convert-all.sh
@@ -7,9 +7,9 @@ if [ $# -ne 3 ]; then
   exit 1
 fi
 
-keyfilepath=$(realpath -s $1)
-srcdirpath=$(realpath -s $2)
-dstdirpath=$(realpath -s $3)
+keyfilepath=$(realpath -s "$1")
+srcdirpath=$(realpath -s "$2")
+dstdirpath=$(realpath -s "$3")
 
 if [[ $keyfilepath != *.k4i ]]; then
   echo "keyfile: $keyfilepath should be end with .k4i" 1>&2
@@ -21,14 +21,14 @@ if [[ ! -d $dstdirpath ]]; then
   echo "dstdirpath: $dstdirpath should be a directory" 1>&2
 fi
 
-for srcfilepath in $(find $srcdirpath -name "*.azw"); do
-  filename_ext=$(basename $srcfilepath)
+find "$srcdirpath" -name "*.azw" | while read srcfilepath; do
+  filename_ext=$(basename "$srcfilepath")
   filename=${filename_ext%.azw}
   dstfilepath=$dstdirpath/${filename}_nodrm.azw3
-  if [[ -e $dstfilepath ]]; then
+  if [[ -e "$dstfilepath" ]]; then
     echo "$srcfilepath is already converted."
   else
     echo "$srcfilepath is converting now ..."
-    ./convert.sh $keyfilepath $srcfilepath $dstfilepath
+    convert.sh "$keyfilepath" "$srcfilepath" "$dstfilepath"
   fi
 done
--- convert.sh
+++ convert.sh
@@ -8,41 +8,41 @@ if [ $# -ne 3 ]; then
 fi
 
 # need absolute path because of docker mount point
-keyfilepath=$(readlink -f $1)
-srcfilepath=$(readlink -f $2)
-dstfilepath=$(readlink -f $3)
+keyfilepath=$(readlink -f "$1")
+srcfilepath=$(readlink -f "$2")
+dstfilepath=$(readlink -f "$3")
 
-if [[ $keyfilepath != *.k4i ]]; then
+if [[ "$keyfilepath" != *.k4i ]]; then
   echo "keyfile: $keyfilepath should be end with .k4i" 1>&2
 fi
-if [[ $srcfilepath != *.azw ]]; then
+if [[ "$srcfilepath" != *.azw ]]; then
   echo "srcfile: $srcfilepath should be end with .azw" 1>&2
 fi
-if [[ $dstfilepath != *.azw3 ]]; then
+if [[ "$dstfilepath" != *.azw3 ]]; then
   echo "dstfile: $dstfilepath should be end with .azw3" 1>&2
 fi
 
-srcdirpath=$(dirname $srcfilepath)
-srcfilename=$(basename $srcfilepath)
-dstdirpath=$(dirname $dstfilepath)
-dstfilename=$(basename $dstfilepath)
+srcdirpath=$(dirname "$srcfilepath")
+srcfilename=$(basename "$srcfilepath")
+dstdirpath=$(dirname "$dstfilepath")
+dstfilename=$(basename "$dstfilepath")
 tmpkeydirpath=$(mktemp -d)
 
 # work around for https://github.com/apprenticeharper/DeDRM_tools/blob/b1d13f2b/src/k4mobidedrm.py#L265-L274
 orig_srcfilename=$srcfilename
 srcfilename=tmpname.azw
-ln -s $orig_srcfilename $srcdirpath/$srcfilename
+ln -s "$orig_srcfilename" "$srcdirpath/$srcfilename"
 
 function cleanup() {
-  if [[ -L $srcdirpath/$srcfilename ]]; then
-    rm $srcdirpath/$srcfilename
+  if [[ -L "$srcdirpath/$srcfilename" ]]; then
+    rm "$srcdirpath/$srcfilename"
   fi
 }
 trap cleanup EXIT
 
-cp $keyfilepath $tmpkeydirpath
+cp "$keyfilepath" "$tmpkeydirpath"
 
 image=k9h5/dedrm-kindle
-docker container run -v $dstdirpath:/root/out -v $srcdirpath:/root/in -v $tmpkeydirpath:/root/.DeDRMPrefs -it $image /root/in/$srcfilename
+docker container run -v "$dstdirpath":"/root/out" -v "$srcdirpath":"/root/in" -v "$tmpkeydirpath":"/root/.DeDRMPrefs" -it $image "/root/in/$srcfilename"
 
-mv $dstdirpath/tmpname_nodrm.azw3 $dstdirpath/$dstfilename
+mv "$dstdirpath/tmpname_nodrm.azw3" "$dstdirpath/$dstfilename"
